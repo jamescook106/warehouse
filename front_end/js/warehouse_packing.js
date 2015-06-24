1f68959e0b6e63fb0022d6c257681e01e29818e5
James Cook <jamescook.106@gmail.com>
2015-06-24 15:57:42 +0100

    Now submit whatever barcode is entered in the barcode

--- front_end/js/warehouse_packing.js
+++ front_end/js/warehouse_packing.js
@@ -1,19 +1,31 @@
+//warehouse_packing.js
+//James Cook
+//This is the JS for the packing page. It takes a harcoded packer ID and submits it with
+//the packing_code
+
 //Create the object to store the json information
 //Not currently in json format
 var json = {};
 
+//The variable for the button press
 var elem = document.getElementById('btSubmit');
 
+//Enables cross site scripting
 $.support.cors = true;
 
+//The hard coded packer_id
 json["Packer_ID"]=1;
-json["Packing_Code"]=2;
 
-//Convert the json variable into JSON
-var json1=JSON.stringify(json);
-//Post the JSON to server
+//Post the JSON to server when the submit button is hit
 elem.onclick=function()
 {
+    //Get the value of the packing_code from the submitted barcode
+    var PC = document.getElementById('PC');
+    json["Packing_Code"]=PC.value;
+    
+    //Convert the json variable into JSON
+    var json1=JSON.stringify(json);
+    
 $.ajax({
         //URL of server
         url: 'http://localhost:4568/packer',
