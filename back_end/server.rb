057fa70faa7b9b2888692ae2215719cab1cb60dc
James Cook <jamescook.106@gmail.com>
2015-06-24 11:29:06 +0100

    Added an allow same origin policy as well as modifing the front end slighlty

--- back_end/server.rb
+++ back_end/server.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require 'json'
 
 post '/packer' do
+    
+    #Allows same origin policy
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    
     params = JSON.parse(request.env["rack.input"].read)
     puts params['Packer_ID']
     puts params['Packing_Code']
