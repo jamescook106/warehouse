5c4ed14dd958a76da08d0c91c97e08e8d47ae86e
James Cook <jamescook.106@gmail.com>
2015-06-29 10:14:48 +0100

    Added the relevant points to the server

--- back_end/server.rb
+++ back_end/server.rb
@@ -9,4 +9,13 @@ post '/packer' do
     params = JSON.parse(request.env["rack.input"].read)
     puts params['Packer_ID']
     puts params['Packing_Code']
+    
+end
+
+get '/shipping' do
+
+end
+
+post 'shipping' do
+
 end
