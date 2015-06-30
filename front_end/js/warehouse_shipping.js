17e1ed86a56b68341dd2752c2aff0dabad92d327
James Cook <jamescook.106@gmail.com>
2015-06-30 12:38:41 +0100

    Added some comments and did some refactoring

--- front_end/js/warehouse_shipping.js
+++ front_end/js/warehouse_shipping.js
@@ -18,6 +18,9 @@ $.support.cors = true;
 //Not currently in json format
 var json = {};
 
+//Once the Shipping code has been entered and submitted
+//We refocus to the Customer ID
+//This also disables the submit on enter function
 $('#SH1').submit(function() {
                  document.getElementById('SH_CI').focus();
                  return false;
@@ -26,16 +29,12 @@ $('#SH1').submit(function() {
 //The submit subroutine
 $('#SH3').on('submit', function(e){
              
+             //If statement to call the get request if the shipping ID and customoer ID match
              if (document.getElementById('SH_CI').value==document.getElementById('SH').value)
              {
              
              //Get the value of the input box before submit
              var SH = document.getElementById("SH");
-             
-             json["Packer_ID"]=PC_ID.value;
-             json["Shipping ID"]=SH.value;
-             json["DT"]=Date.now();
-             var json1=JSON.stringify(json);
              
              //Variable for the GET request with textbox entry value
              var url_str = 'http://localhost:4568/shipping/' +SH.value;
@@ -51,40 +50,48 @@ $('#SH3').on('submit', function(e){
                     success: function(data) {document.getElementById('test').innerHTML = data;}
                     });
              
-             
-             
              //Refocus the user back to the input form
              document.getElementById('SH_CF').focus();
              }
              else
              {
+             //Call an alert if customer ID and Box ID do not match
              alert("Customer ID does not match box code");
+             
+             //Refocus the box accordingly
              document.getElementById('SH').value = "";
              document.getElementById('SH').focus();
              document.getElementById('SH_CI').value = "";
+             
+             //Stop submit behaviour
              return false;
              }
              
              });
 
+//The confrim subroutine after the customisations have been packed
 $('#SH2').submit(function() {
                  
+                 //Submit the Post only if the confirm code has been scanned
                  if (document.getElementById('SH_CF').value==3131910018889)
                  {
                  var SH = document.getElementById("SH");
                  
+                 //Set up the json to include the shipping ID, Packer ID, and Date
                  json["Packer_ID"]=PC_ID.value;
                  json["Shipping ID"]=SH.value;
                  json["DT"]=Date.now();
+                 
+                 //Convert the json variable into actual json
                  var json1=JSON.stringify(json);
                  
+                 //Refocus the box accordingly
                  document.getElementById('SH').focus();
-                 //Clear the input field
                  document.getElementById('SH').value = "";
-                 //Clear the input field
                  document.getElementById('SH_CF').value = "";
                  document.getElementById('SH_CI').value = "";
                  
+                 //Submit the Post request
                  $.ajax({
                         url: 'http://localhost:4568/shipping',
                         //Post Method
@@ -96,11 +103,12 @@ $('#SH2').submit(function() {
                  document.getElementById('test').innerHTML=""
                  }
                  else{
-                 alert("FALSE");
+                 //Alert to say that the code scanned was not the confirmation code
+                 alert("Confirmation code not scanned");
                  document.getElementById('SH_CF').focus();
                  document.getElementById('SH_CF').value = "";
                  }
                 
-                 
+                 //Stop the form submitting
                  return false;
                  });
