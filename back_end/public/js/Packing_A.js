2bc2f38ee849f4995c9ad9cc86b54b3f34c7e15d
James Cook <jamescook.106@gmail.com>
2015-07-10 13:36:37 +0100

    Tidied up the Packing angular, it is now fully converted!

--- back_end/public/js/Packing_A.js
+++ back_end/public/js/Packing_A.js
@@ -1,17 +1,17 @@
 var MyApp=angular.module("packing_app", []);
 
-     MyApp.controller("packer_id_controller", function($scope,$rootScope, $http) {
+  MyApp.controller("packer_id_controller", function($scope,$rootScope, $http) {
         
-      $("#packer_id").focus();
-      $scope.myForm = {};
-      $scope.myForm.packer_id="";
+    $("#packer_id").focus();
+    $scope.myForm = {};
+    $scope.myForm.packer_id="";
+    
+    $scope.myForm.submitPackerId = function(item, event) {
 
-     $scope.myForm.submitPackerId = function(item, event) {
-
-       console.log("--> Submitting Packing ID");
-       var packer_id_var = './packer/'+ $scope.myForm.packer_id;
-       console.log(packer_id_var);
-       $rootScope.packer_id=$scope.myForm.packer_id;
+      console.log("--> Submitting Packing ID");
+      var packer_id_var = './packer/'+ $scope.myForm.packer_id;
+      console.log(packer_id_var);
+      $rootScope.packer_id=$scope.myForm.packer_id;
 
       $http.get(packer_id_var)
       .success(function(data, status, headers, config) {
@@ -27,23 +27,26 @@ var MyApp=angular.module("packing_app", []);
           if (data!=404){
             $scope.message="Unknown error";
           }
-  });
-     }
-
-  });
-
-MyApp.controller("packing_id_controller", function($scope,$rootScope, $http) {
-  $scope.myForm = {};
-  $scope.myForm.submitPackingId = function(item, event) {
-    console.log("--> Submitting Packing ID");
-    var dataObj={packing_id:$scope.myForm.packing_id, packer_id:$rootScope.packer_id};
-    $http.post('/packer', dataObj)
-      .success(function(data, status, headers, config) {
-        $scope.message=data;
-      })
-      .error(function(data, status, headers, config) {
       });
+    }
+
+  });
+
+  MyApp.controller("packing_id_controller", function($scope,$rootScope, $http) {
+
+    $scope.myForm = {};
     $scope.myForm.packing_id="";
-    $("#packing_id").focus();    
-  };
-});
+
+    $scope.myForm.submitPackingId = function(item, event) {
+      console.log("--> Submitting Packing ID");
+      var dataObj={packing_id:$scope.myForm.packing_id, packer_id:$rootScope.packer_id};
+      $http.post('/packer', dataObj)
+        .success(function(data, status, headers, config) {
+        })
+        .error(function(data, status, headers, config) {
+        });
+
+      $scope.myForm.packing_id="";
+      $("#packing_id").focus();    
+    };
+  });
