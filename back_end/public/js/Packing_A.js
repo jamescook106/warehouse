91e57a1411431e2cb78ea5dfbe1fecd30098954d
James Cook <jamescook.106@gmail.com>
2015-07-10 14:28:03 +0100

    Converting shipping to angular

--- back_end/public/js/Packing_A.js
+++ back_end/public/js/Packing_A.js
@@ -1,6 +1,6 @@
-var MyApp=angular.module("packing_app", []);
+var P_A=angular.module("packing_app", []);
 
-  MyApp.controller("packer_id_controller", function($scope,$rootScope, $http) {
+  P_A.controller("packer_id_controller", function($scope,$rootScope, $http) {
         
     $("#packer_id").focus();
     $scope.myForm = {};
@@ -32,7 +32,7 @@ var MyApp=angular.module("packing_app", []);
 
   });
 
-  MyApp.controller("packing_id_controller", function($scope,$rootScope, $http) {
+  P_A.controller("packing_id_controller", function($scope,$rootScope, $http) {
 
     $scope.myForm = {};
     $scope.myForm.packing_id="";
@@ -48,5 +48,56 @@ var MyApp=angular.module("packing_app", []);
 
       $scope.myForm.packing_id="";
       $("#packing_id").focus();    
+    };
+  });
+
+var S_A=angular.module("shipping_app", []);
+
+S_A.controller("packer_id_controller", function($scope,$rootScope, $http) {
+        
+    $("#packer_id").focus();
+    $scope.myForm = {};
+    $scope.myForm.packer_id="";
+    
+    $scope.myForm.submitPackerId = function(item, event) {
+
+      console.log("--> Submitting Packing ID");
+      var packer_id_var = './packer/'+ $scope.myForm.packer_id;
+      console.log(packer_id_var);
+      $rootScope.packer_id=$scope.myForm.packer_id;
+
+      $http.get(packer_id_var)
+      .success(function(data, status, headers, config) {
+        $scope.message=data;
+        console.log(data);
+        $("#shipping_id").focus();
+        })
+      .error(function(data, status, headers, config) {
+          console.log(status);
+          if (data=404){
+            $scope.message="Blank Packer ID submitted";
+          }
+          if (data!=404){
+            $scope.message="Unknown error";
+          }
+      });
+    }
+
+  });
+
+S_A.controller("shipping_id_controller", function($scope,$rootScope, $http) {
+
+    $scope.myForm = {};
+    $scope.myForm.shipping_id="";
+
+    $scope.myForm.submitShippingId = function(item, event) {
+      console.log("--> Submitting Packing ID");
+      var shipping_id_var = './shipping/'+ $scope.myForm.shipping_id;
+      $http.get(shipping_id_var)
+      .success(function(data, status, headers, config) {
+        $scope.message=data;
+        console.log(data);
+        })
+      .error(function(data, status, headers, config) {});  
     };
   });
